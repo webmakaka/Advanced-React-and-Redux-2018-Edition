e79cddbf2db4c6ee2b172e6768dffc48c55ecf5a
marley-react <marley.react@gmail.com>
2018-11-02 18:12:36 +0300

    Testing

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # [Stephen Grider]  Advanced React and Redux: 2018 Edition [Udemy, ENG, 2018]
 
+Original src:  
+https://github.com/StephenGrider/AdvancedReduxCode  
+https://github.com/StephenGrider/ReduxSimpleStarter
+
+
+    $ npx create-react-app .
+
+<br/>
+
+## Testing
+
+    $ npm install --save redux react-redux
+
+    $ npm install --save-dev enzyme enzyme-adapter-react-16
+
+    $ npm install --save axios redux-promise moxios
+
 
 ---
 
---
+++ src/components/__tests__/CommentList.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import CommentList from '../CommentList';
+import Root from 'Root';
+
+let wrapped;
+
+beforeEach(() => {
+
+  const initialState = {
+    comments: ['Comment 1', 'Comment 2', 'Comment 3']
+  }
+
+  wrapped = mount(
+    <Root initialState={initialState} >
+      <CommentList />
+    </Root>
+  );
+});
+
+it('creates one LI per comment', () => {
+  expect(wrapped.find('li').length).toEqual(3);
+});
+
+it('shows the text for each comment', () => {
+  expect(wrapped.render().text()).toContain('Comment 1');
+  expect(wrapped.render().text()).toContain('Comment 2');
+  expect(wrapped.render().text()).toContain('Comment 3');
+});
